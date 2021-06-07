42f6b753e5c50b3b6e3ed1005e48d6467507ff9e
TiannaLopes <tiannalopes@shaw.ca>
2021-06-07 11:07:58 -0400

    newhw

---
+++ script.js
@@ -0,0 +1,57 @@
+// Task 1
+// Given a string of odd characters with lengths more than 5 and less than 50. 
+//Write a function to extract the middle 3 characters
+// Input : “aacjddfdd“
+// output : jdd
+
+function task1(str){
+ 
+
+}
+
+//document.getElementByClass('answer1').innerHTML = ('Banana', task1('banana'));
+
+
+// Task 2
+// Write a function to return if the number is a prime number or not
+// Input between- 1 to 100
+// Output should be in array , First number tell if its prime number or not and second output tell the next prime number moving towards 100
+// For example
+// Input : 47  Output ; [true , 53]
+// Since 47 is prime number , we returned true and next prime number moving towards 100 is 53
+
+function task2(num){
+
+}
+//document.getElementByClass('answer2').innerHTML = ();
+
+
+// Task 3
+// Write a function that calculates a number of digits.This challenge requires you to calculate the run-length encoding of a string. 
+//This compresses the string in the following way: wherever there are multiple repeating characters in a string, replace them with the character count along with the original character. 
+//For example: the string "aaabbc" results in an encoding of "3a2b1c" You can see that the count comes first followed by the character.
+
+function task3(){
+
+}
+
+
+//document.getElementByClass('answer3').innerHTML = ();
+
+// Task 4 
+// Given a string of input of word consisting entirely of alphabets .
+// For each alphabet in input , you have to do two things
+// change alphabet to next alphabet – Ex “a” become “b” , “d” become “e”
+// If its lower case it becomes uppercase and vice versa
+// Ex Input - chIme
+// Output-- DImNF
+// first letter c become capital D
+// second letter h becomes capital I
+// third l become small m
+
+function task4(str){
+
+}
+
+
+//document.getElementByClass('answer4').innerHTML = ();
