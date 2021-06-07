86ce8e4dff478297cdc0fb6d4eb58192de9d6b63
TiannaLopes <tiannalopes@shaw.ca>
2021-06-07 16:43:38 -0400

    workedon

--- script.js
+++ script.js
@@ -5,10 +5,12 @@
 // output : jdd
 
 function task1(str){
- 
+ let len = str.length;
+ let middle = len / 2;
+ console.log(str.charAt(middle));
 
 }
-
+task1('wwordd');
 //document.getElementByClass('answer1').innerHTML = ('Banana', task1('banana'));
 
 
@@ -21,9 +23,40 @@ function task1(str){
 // Since 47 is prime number , we returned true and next prime number moving towards 100 is 53
 
 function task2(num){
-
+    for(var i = 2; i < num; i++){
+    if(num % i === 0) {
+        return false;
+    }
+    return (num>1) ; 
 }
+}
+console.log(task2(34));
+console.log(task2(33));
+
+// work in progress 
+function task22(num){
+    var prime = true;
+    for(i=1;i<=100;i++) { 
+        prime = true; 
+        for(j=2;j<i;j++) { 
+            if(i%j===0) { 
+                prime = false; 
+                break; 
+            } } 
+            if(prime === true) {
+                 let ar = [i]; 
+                
+
+    if (ar.includes('num')){
+        let nextPrime = i + 1;
+        console.log(ar(i), nextPrime);
+        }
+    }
+            }}
+task22(23);
+
 //document.getElementByClass('answer2').innerHTML = ();
+
 
 
 // Task 3
@@ -31,9 +64,22 @@ function task2(num){
 //This compresses the string in the following way: wherever there are multiple repeating characters in a string, replace them with the character count along with the original character. 
 //For example: the string "aaabbc" results in an encoding of "3a2b1c" You can see that the count comes first followed by the character.
 
-function task3(){
+function task3(str){
+        let n = str.length;
+        for (let i = 0; i < n; i++)
+        {
+         let count = 1;
+            while (i < n - 1 && str[i] == str[i+1])
+            {
+                count++;
+                i++;
+            }
+        console.log(count,str[i]);
+ 
+        }
+    }
+    task3("aaabbc");
 
-}
 
 
 //document.getElementByClass('answer3').innerHTML = ();
@@ -54,4 +100,4 @@ function task4(str){
 }
 
 
-//document.getElementByClass('answer4').innerHTML = ();
+//document.getElementByClass('answer4').innerHTML = ()
